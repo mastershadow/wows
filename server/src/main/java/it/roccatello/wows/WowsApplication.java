ce2d4a19e627e7f4f1ea098aeb46b6a34cb66793
Eduard Roccatello <eduard.roccatello@3dgis.it>
2025-02-15 14:32:51 +0000

    setup

---
+++ server/src/main/java/it/roccatello/wows/WowsApplication.java
@@ -0,0 +1,13 @@
+package it.roccatello.wows;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WowsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WowsApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/it/roccatello/wows/WowsApplicationTests.java
@@ -0,0 +1,13 @@
+package it.roccatello.wows;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WowsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
