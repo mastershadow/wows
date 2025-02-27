872cff099fcad93e85434f0986c03a370ca64578
Eduard Roccatello <eduard.roccatello@3dgis.it>
2025-02-27 10:52:36 +0000

    dev

--- server/src/main/java/it/roccatello/wows/service/broker/BitvavoService.java
+++ server/src/main/java/it/roccatello/wows/service/broker/BitvavoService.java
@@ -4,10 +4,17 @@ import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
 import it.roccatello.wows.model.db.Provider;
+import lombok.Getter;
+import lombok.Setter;
 
 @Lazy
 @Service
 public class BitvavoService extends BrokerService {
+  @Getter @Setter
+  private String apiKey;
+
+  @Getter @Setter
+  private String apiSecret;
   
   @Override
   public String getCode() {
@@ -16,5 +23,7 @@ public class BitvavoService extends BrokerService {
 
   @Override
   public void configure(Provider provider) {
+    this.apiKey = String.valueOf(provider.getAttrs().get("key"));
+    this.apiSecret = String.valueOf(provider.getAttrs().get("secret"));
   }
 }
