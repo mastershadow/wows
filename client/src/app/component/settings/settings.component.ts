6948c5b7f8fb324e660fa33e765c81120d51f4ce
Eduard Roccatello <eduard.roccatello@3dgis.it>
2025-02-28 15:12:07 +0000

    dev

--- client/src/app/component/settings/settings.component.ts
+++ client/src/app/component/settings/settings.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+export interface SettingsEntry {
+  key: string;
+  value: string;
+}
 
+const ELEMENT_DATA: SettingsEntry[] = [
+  {key: '1', value: 'Hydrogen'},
+  {key: '2', value: 'Helium'},
+  {key: '3', value: 'Lithium'},
+  {key: '4', value: 'Beryllium'},
+  {key: '5', value: 'Boron'},
+  {key: '6', value: 'Carbon'},
+  {key: '7', value: 'Nitrogen'},
+  {key: '8', value: 'Oxygen'},
+  {key: '9', value: 'Fluorine'},
+  {key: '10', value: 'Neon'}
+];
 @Component({
   selector: 'app-settings',
-  imports: [],
+  imports: [MatTableModule],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.scss'
 })
 export class SettingsComponent {
+  displayedColumns: string[] = ['key', 'value'];
+  dataSource = ELEMENT_DATA;
 
 }
