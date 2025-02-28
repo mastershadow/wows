d0da1930cb15aa5d0d73f91cab525b65d2f9c5f5
Eduard Roccatello <eduard.roccatello@3dgis.it>
2025-02-28 10:10:16 +0000

    dev

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,20 +3,20 @@ import { AuthService } from './service/auth.service';
 import { inject } from '@angular/core';
 import { LoginComponent } from '@/component/login/login.component';
 import { LogoutComponent } from '@/component/logout/logout.component';
-import { BalanceComponent } from '@/component/balance/balance.component';
 import { TransactionsComponent } from '@/component/transactions/transactions.component';
-import { AssetsComponent } from '@/component/assets/assets.component';
 import { TradesComponent } from './component/trades/trades.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { TickersComponent } from './component/tickers/tickers.component';
 
 const isLoggedGuard: CanActivateFn = (route, state) => { return inject(AuthService).isLoggedIn() };
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'logout', component: LogoutComponent, canActivate: [ isLoggedGuard ] },
-  { path: 'balance', component: BalanceComponent, canActivate: [ isLoggedGuard ] },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [ isLoggedGuard ] },
   { path: 'trades', component: TradesComponent, canActivate: [ isLoggedGuard ] },
   { path: 'transactions', component: TransactionsComponent, canActivate: [ isLoggedGuard ] },
-  { path: 'assets', component: AssetsComponent, canActivate: [ isLoggedGuard ] },
+  { path: 'tickers', component: TickersComponent, canActivate: [ isLoggedGuard ] },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: '**', redirectTo: '/login' },
 ];
---
+++ client/src/app/component/tickers/tickers.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tickers',
+  imports: [],
+  templateUrl: './tickers.component.html',
+  styleUrl: './tickers.component.scss'
+})
+export class TickersComponent {
+
+}
