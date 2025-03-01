09ce92521581f2fa13df76ff602fb84bd4a4070c
Eduard Roccatello <eduard.roccatello@3dgis.it>
2025-03-01 21:53:58 +0000

    dev

---
+++ server/src/main/java/it/roccatello/wows/model/db/IndicatorData.java
@@ -0,0 +1,28 @@
+package it.roccatello.wows.model.db;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class IndicatorData extends BaseDbModel {
+  @ManyToOne
+  private Indicator indicator;
+  
+  @ManyToOne
+  private Ticker ticker;
+
+  private Long start;
+
+  private Long end;
+
+  @Column(precision = 32, scale = 10)
+  private List<BigDecimal> data;
+}
