ff3c2c9f4f7d84ce4f40bf76cd5f23e72d429a1e
Eduard Roccatello <eduard.roccatello@3dgis.it>
2025-03-05 14:54:33 +0000

    test stubs

--- server/src/test/java/it/roccatello/wows/processing/CandleProcessingTests.java
+++ server/src/test/java/it/roccatello/wows/processing/CandleProcessingTests.java
@@ -1,4 +1,4 @@
-package it.roccatello.wows;
+package it.roccatello.wows.processing;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,7 +6,7 @@ import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
-class WowsApplicationTests {
+class CandleProcessingTests {
 
 	@Test
 	void contextLoads() {
